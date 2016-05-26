c143ebb475193e283e4498586dbf81e79c282dc3
mghadley <matthew.g.hadley@gmail.com>
2016-05-26 13:43:37 -0600

    set up models and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'trips/index'
+
+  get 'trips/show'
+
+  get 'trips/new'
+
+  get 'trips/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
