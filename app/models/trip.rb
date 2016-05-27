1eeacb68e4e4ab7637ea1aefed531fde57a7a246
mghadley <matthew.g.hadley@gmail.com>
2016-05-27 09:21:03 -0600

    added date validation

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,5 +2,12 @@ class Trip < ActiveRecord::Base
 	has_many :locations, dependent: :destroy
 	validates_presence_of :title
 	validates :budget, :length, numericality: { greater_than: 0 }
-	# validates :date, greater_than_or_equal_to: Time.now
+	validate :trip_date_cannot_be_in_the_past,
+
+ 
+  def trip_date_cannot_be_in_the_past
+    if date.present? && date < Date.today
+      errors.add(:start_date, "can't be in the past")
+    end
+  end
 end
